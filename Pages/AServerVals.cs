eee395fd07a2d28a71b5061f4043cee79ca6434e
Tayron Jimenez <tayrom@gmail.com>
2024-06-03 11:04:47 -0600

    Update AServerVals.cs

--- Pages/AServerVals.cs
+++ Pages/AServerVals.cs
@@ -8,11 +8,11 @@ namespace commPinPad.Pages
 {
     public class AServerVals
     {
-        public const string url =  "http://74.208.210.198/send_Code200.php";
+        public const string url =  "http://0.0.0.0/send_Code200.php";
 
-        public const string url2 = "http://74.208.210.198/send_Code200_compra.php";
+        public const string url2 = "http://0.0.0.0/send_Code200_compra.php";
 
-        public const string url3 = "http://74.208.210.198/send_Code200_llave.php";
+        public const string url3 = "http://0.0.0.0/send_Code200_llave.php";
 
         public AServerVals()
         {
